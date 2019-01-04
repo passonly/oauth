c3cc764a5048da68f64b139c4cae54b1a5cedcba
likun <passonly@163.com>
2019-01-04 17:54:33 +0800

    first

---
+++ src/main/java/com/oauth/Application.java
@@ -0,0 +1,18 @@
+package com.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: likun.
+ * @Description:
+ * @Date:Created in 2019/1/4 10:26.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Application.class, args);
+    }
+}
